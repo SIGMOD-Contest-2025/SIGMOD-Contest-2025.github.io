a29c1fe9e948d6865a4c5800cfaa6d536baa1630
BenjaminXiang <xiangl3@mail.sustech.edu.cn>
2024-12-18 15:58:24 +0800

    delete php

--- dashboard/mysql.config.php
+++
@@ -1,7 +0,0 @@
-<?php //db credentials
-	$db_salt="fe423HJe2jd"; //salt, for tastier passwords
-	$db_host="db";	// Host name
-	$db_username="sigmod";	// Mysql username
-	$db_password="D1Wjk@A&MLP"; // Mysql password
-	$db_name="sigmod";	// Database name
-?>
